f5c466d9a220d7fec6b31265cd53e86aa3840235
Justin Juno <hello@juno.codes>
2018-12-16 17:18:29 -0600

    update description info

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,28 @@ import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={['justin', 'juno', 'junocodes', 'react', 'texas', 'developer', 'javascript', 'react', 'graphql', 'd3', 'engineer', 'code']} />
+    <SEO
+      title="Home"
+      keywords={[
+        'justin',
+        'juno',
+        'junocodes',
+        'react',
+        'texas',
+        'developer',
+        'javascript',
+        'react',
+        'graphql',
+        'd3',
+        'engineer',
+        'code',
+      ]}
+    />
     <h1>Hello World!</h1>
     <p>
       My name is Justin Juno. I'm an infinitely curious javascript developer
-      from Whitehouse, Texas. Currently revamping my portfolio and exploring
-      React, GraphQL, and D3. Proud supporter of Styled Components. Probably the
-      coolest dad of all-time.
+      from Whitehouse, Texas. Currently working on my portfolio and exploring
+      React, D3, and GraphQL. Striving to be the coolest dad of all-time.
     </p>
   </Layout>
 )
